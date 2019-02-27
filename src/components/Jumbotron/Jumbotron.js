dfe7941e290c255db434c46ace9f150e30a37012
Tomasz Dudzinski <tomasz.j.p.dudzinski@gmail.com>
2019-02-27 23:32:43 +0100

    Content and Jumbotron Components

---
+++ src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,27 @@
+import React from "react"
+import image from "./../../images/image1.jpg"
+import styled from "styled-components"
+import theme from "./../../utils/theme"
+
+const StyledSection = styled.section`
+  ${theme.flexWrapper()};
+  width: 100%;
+  height: 23.3rem;
+  background: url(${image}) no-repeat;
+  background-size: cover;
+`
+const StyledH1 = styled.h1`
+  font: 400 4rem ${theme.fonts.primaryFont};
+  color: ${theme.colors.headerColor};
+  text-transform: uppercase;
+`
+
+const Jumbotron = ({ text }) => {
+  return (
+    <StyledSection>
+      <StyledH1>{text}</StyledH1>
+    </StyledSection>
+  )
+}
+
+export default Jumbotron
