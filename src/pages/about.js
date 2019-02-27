d4a7314df3d382c24e6ad28b7f8544de5614b6ad
Tomasz Dudzinski <tomasz.j.p.dudzinski@gmail.com>
2019-02-27 16:44:30 +0100

    Added pages to the project

---
+++ src/pages/about.js
@@ -0,0 +1,6 @@
+import React from "react"
+import Layout from "../layout/layout"
+
+const About = () => <Layout />
+
+export default About
---
+++ src/pages/contact.js
@@ -0,0 +1,6 @@
+import React from "react"
+import Layout from "./../layout/layout"
+
+const Contact = () => <Layout />
+
+export default Contact
